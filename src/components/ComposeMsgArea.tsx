6b5e7d81373a6eae2a3e1b7399d430a5ef58614c
Kurtis Grant <kurtis.b.grant@gmail.com>
2023-09-19 18:03:32 -0400

    Styling tweak

--- src/components/ComposeMsgArea.tsx
+++ src/components/ComposeMsgArea.tsx
@@ -19,7 +19,7 @@ export default function ComposeMsgArea({ sendMessage }: { sendMessage: (message:
     };
 
     return (
-        <form onSubmit={handleSendMessage} className="flex gap-4 bg-purple-900 p-2 justify-center rounded-b-xl">
+        <form onSubmit={handleSendMessage} className="flex gap-4 bg-[hsl(280,100%,40%)] p-2 justify-center rounded-b-xl">
             <textarea
                 className="rounded-md p-1 w-full bg-grey-300"
                 value={message}
@@ -28,7 +28,7 @@ export default function ComposeMsgArea({ sendMessage }: { sendMessage: (message:
             />
             <button
                 type="submit"
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20 self-end"
+                className="rounded-full bg-white/20 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/30 self-end"
             >
                 Send
             </button>
