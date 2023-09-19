f8529b0c2b3c8f6ff3de09948d87817fce65f114
Kurtis Grant <kurtis.b.grant@gmail.com>
2023-09-19 12:25:20 -0400

    Style updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           </h1>
           <div className="flex flex-col items-center gap-2">
             <div className="flex flex-col items-center justify-center gap-4">
-              <div className="w-full h-1/2 bg-gray-200 rounded-lg opacity-50">
+              <div className="w-full h-1/2 bg-gray-100 rounded-lg">
                 <Messages messages={messages} />
                 <ComposeMsgArea sendMessage={submitUserMessage} />
               </div>
@@ -78,21 +78,21 @@ export default function Home() {
 
 function Messages({ messages }: { messages: Message[] }) {
   return (
-    <div className="flex flex-col gap-4 p-2">
+    <div className="flex flex-col p-2">
       {messages.map((message, index) => (
         <div
           key={index}
           className={`flex ${message.type === 'ai' ? 'justify-start' : 'justify-end'}`}
         >
           <div className={`flex flex-col w-4/5`}>
-            <div className={`rounded-md p-1 ${message.type === 'ai' ? 'self-start bg-gray-300' : 'self-end bg-blue-300'}`}>
-              <p className={`font-bold ${message.type === 'ai' ? 'text-left' : 'text-right'}`}>
-                {message.sender}:
-              </p>
+            <div className={`rounded-xl px-5 py-3 ${message.type === 'ai' ? 'self-start bg-gray-300 rounded-bl-none' : 'self-end bg-blue-300 rounded-br-none'}`}>
               <p className={`${message.type === 'ai' ? 'self-start' : 'self-end'}`}>
                 {message.content}
               </p>
             </div>
+            <p className={`italic px-2 ${message.type === 'ai' ? 'text-left' : 'text-right'}`}>
+              {message.sender}
+            </p>
           </div>
         </div>
       ))}
