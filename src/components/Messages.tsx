e8c2b95dfc298e76c045300e68514cc4a285db91
Kurtis Grant <kurtis.b.grant@gmail.com>
2023-10-01 06:34:23 -0400

    Improve styling and update project name

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -2,20 +2,20 @@ import { Message } from "~/types";
 
 export default function Messages({ messages, deleteMessage }: { messages: Message[], deleteMessage: (index: string) => void }) {
     return (
-        <div className="flex flex-col p-2 h-[50vh] overflow-auto">
+        <div className="flex flex-col p-3 h-[64vh] gap-1 overflow-auto">
             {messages?.map((message) => (
                 <div
                     key={message.id}
                     className={`flex ${message.type === 'assistant' ? 'justify-start' : 'justify-end'}`}
                 >
-                    <div className={`flex flex-col w-4/5`}>
-                        <div className={`rounded-xl px-5 py-3 ${message.type === 'assistant' ? 'self-start bg-gray-300 rounded-bl-none' : 'self-end bg-blue-300 rounded-br-none'}`}>
+                    <div className={`flex flex-col w-4/5 relative`}>
+                        <div className={`rounded-xl text-sm px-5 py-2 ${message.type === 'assistant' ? 'self-start bg-gray-300 rounded-bl-none' : 'self-end bg-blue-300 rounded-br-none'}`}>
                             <p className={`${message.type === 'assistant' ? 'self-start' : 'self-end'}`}>
                                 {message.content}
                             </p>
-                            <button onClick={() => message.id && deleteMessage(message.id)} className="self-end opacity-20 hover:opacity-60">x</button>
                         </div>
-                        <p className={`italic px-2 ${message.type === 'assistant' ? 'text-left' : 'text-right'}`}>
+                        <button onClick={() => message.id && deleteMessage(message.id)} className="absolute top-0 right-0 m-2 opacity-20 hover:opacity-60">x</button>
+                        <p className={`italic text-xs px-1 ${message.type === 'assistant' ? 'text-left' : 'text-right'}`}>
                             {message.sender} {message.timestamp?.toLocaleString()}
                         </p>
                     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ export default function Home() {
   const { data: messages, refetch } = api.endpoints.getMessages.useQuery();
 
   const addMessageMutation = api.endpoints.addMessage.useMutation();
-
   const deleteMessageMutation = api.endpoints.deleteMessage.useMutation();
 
   const submitUserMessage = async (message: string) => {
@@ -36,7 +35,7 @@ export default function Home() {
     const reply = await response.json();
 
     const newResponse: Message = {
-      sender: 'Sarge',
+      sender: 'Builder Bot',
       content: reply.response,
       type: 'assistant'
     }
@@ -51,31 +50,33 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Sarge</title>
+        <title>getbuilding</title>
         <meta name="description" content="An accountability coach chatbot driven by AI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-12">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Sarge<span className="text-[hsl(280,100%,70%)]">.ai</span>
+      <main className="flex min-h-screen flex-col items-center justify-around bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container h-full flex flex-col items-center justify-around gap-5 px-4 py-2">
+          <h1 className="text-4xl font-extrabold tracking-tight text-white mt-0 mb-auto">
+            getbuilding<span className="text-[hsl(280,100%,70%)]">.ai</span>
           </h1>
           <div className="flex flex-col items-center gap-2 w-full">
             <div className="flex flex-col items-center justify-center gap-4 w-full">
-              <div className="w-full h-1/2 bg-gray-100 rounded-xl">
+              <div className="w-full h-3/4 bg-gray-100 rounded-xl">
                 <Messages messages={messages as Message[]} deleteMessage={deleteMessage} />
                 <ComposeMsgArea sendMessage={submitUserMessage} />
               </div>
 
-              <p className="text-center text-2xl text-white">
-                {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-              </p>
-              <button
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-                onClick={sessionData ? () => void signOut() : () => void signIn()}
-              >
-                {sessionData ? "Sign out" : "Sign in"}
-              </button>
+              <div className="flex items-center justify-center">
+                <p className="text-center text-l text-white mr-4">
+                  {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+                </p>
+                <button
+                  className="rounded-full bg-white/10 px-10 py-2 font-semibold text-white no-underline transition hover:bg-white/20"
+                  onClick={sessionData ? () => void signOut() : () => void signIn()}
+                >
+                  {sessionData ? "Sign out" : "Sign in"}
+                </button>
+              </div>
             </div>
           </div>
         </div>
